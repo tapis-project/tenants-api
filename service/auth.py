8d529ab9daf876f9f59972ba603d84b00ef6ce24
Joe Stubbs <jstubbs@tacc.utexas.edu>
2019-11-13 13:06:56 -0600

    check for the tenant_creator role when use_sk config is true.

--- service/auth.py
+++ service/auth.py
@@ -1,7 +1,9 @@
 from flask import g, request
 
 from common import auth
+from common.config import conf
 from common import errors as common_errors
+from tapy.dyna import DynaTapy
 
 # get the logger instance -
 from common.logs import get_logger
@@ -30,17 +32,32 @@ def authentication():
     except common_errors.NoTokenError as e:
         logger.debug(f"Caught NoTokenError: {e}")
         g.no_token = True
+        g.username = None
+        g.tenant_id = None
         # for retrieval and informational methods, allow the request (with possibly limited information)
         if request.method == 'GET' or request.method == 'OPTIONS' or request.method == 'HEAD':
             return True
         raise e
 
 
+# this role is stored in the security kernel
+ROLE = 'tenant_creator'
+
+# this is the Tapis client that tenants will use for interacting with other services, such as the security kernel.
+t = auth.get_service_tapy_client()
+
 def authorization():
     """
     Entry point for checking authorization for all requests to the authenticator.
     :return:
     """
-    # todo - Call security kernel to check if user is authorized for the request.
-    #
-    return True
+    if not conf.use_sk:
+        return True
+    if request.method == 'GET' or request.method == 'OPTIONS' or request.method == 'HEAD':
+        return True
+    # check to make sure the user has the necessary role. -the tenant to check in is based on the tenant being
+    # "served" by this instance of the
+    users = t.sk.getUsersWithRole(roleName=ROLE)
+    if g.username not in users.names:
+        raise common_errors.PermissionsError(msg='Not authorized to modify the registry of tenants.')
+
