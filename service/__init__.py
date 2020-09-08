ddde0ad34892c85256cbf29ecf7a6e8aa9d68fdc
Joe Stubbs <joestubbs5@gmail.com>
2020-09-08 17:02:19 -0500

    convert to using postgres config in individual parts (user, password, url) instead of a single, complete config.

--- service/__init__.py
+++ service/__init__.py
@@ -3,9 +3,19 @@ from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
+# get the logger instance -
+from common.logs import get_logger
+logger = get_logger(__name__)
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = conf.sql_db_url
+
+try:
+    full_db_url = f'postgres://{conf.postgres_user}:{conf.postgres_password}@{conf.sql_db_url}'
+except Exception as e:
+    logger.error(f"Got exception trying to build full_db_ulr; e: {e}")
+    raise e
+
+app.config['SQLALCHEMY_DATABASE_URI'] = full_db_url
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
